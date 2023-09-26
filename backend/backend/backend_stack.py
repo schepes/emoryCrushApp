3ecf5eacc458375ef2a095af103491ec54730a96
Diego Bobrow <diegobobrow25@gmail.com>
2023-09-26 04:00:46 -0400

    fix: fixed stacks version

--- backend/backend/backend_stack.py
+++ backend/backend/backend_stack.py
@@ -1,19 +1,58 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_dynamodb as dynamodb,
+    aws_cognito as cognito,
 )
 from constructs import Construct
 
-class BackendStack(Stack):
+class CrushEmoryBackendStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Import existing Cognito User Pool
+        user_pool = cognito.UserPool.from_user_pool_id(
+            self,
+            "ExistingUserPool",
+            "us-east-1_9MHY29BIx"  # Replace with your actual User Pool ID
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "BackendQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # DynamoDB table for all Users
+        users_table = dynamodb.Table(
+            self, "UsersTable",
+            partition_key=dynamodb.Attribute(
+                name="userId",
+                type=dynamodb.AttributeType.STRING
+            )
+        )
+
+        # DynamoDB table for Likes
+        likes_table = dynamodb.Table(
+            self, "LikesTable",
+            partition_key=dynamodb.Attribute(
+                name="userId",
+                type=dynamodb.AttributeType.STRING
+            ),
+            sort_key=dynamodb.Attribute(
+                name="likedUserId",
+                type=dynamodb.AttributeType.STRING
+            )
+        )
+
+        # DynamoDB table for Friends
+        friends_table = dynamodb.Table(
+            self, "FriendsTable",
+            partition_key=dynamodb.Attribute(
+                name="userId",
+                type=dynamodb.AttributeType.STRING
+            )
+        )
+
+        # DynamoDB table for Matches
+        matches_table = dynamodb.Table(
+            self, "MatchesTable",
+            partition_key=dynamodb.Attribute(
+                name="userId",
+                type=dynamodb.AttributeType.STRING
+            )
+        )
